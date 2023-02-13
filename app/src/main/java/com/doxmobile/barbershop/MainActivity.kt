a13ebca8b8853f5a6c3618e1e055ec5d3613febd
Davron <davronkholboev122@gmail.com>
2023-02-13 18:42:38 +0500

    feat:added libraries and modules

--- app/src/main/java/com/doxmobile/barbershop/MainActivity.kt
+++ app/src/main/java/com/doxmobile/barbershop/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.doxmobile.barbershop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.barbershop.R
 
 class MainActivity : AppCompatActivity() {
